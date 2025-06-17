a139efd87ae9bd89c6016788ac6a51507e894620
Rakshitih <rakshithkumarb089@gmail.com>
2025-06-17 23:13:10 +0530

    Update work.py

--- working.py/work.py
+++ working.py/work.py
@@ -5,6 +5,9 @@
 
 
 letter = """ Dear Rakshith,
+We are pleased to inform you that you have been selected for the <role> position at our company. Your skills and experience are a perfect match for our team.
+We look forward to working with you and contributing to our success together.
+       Best regards,
          12/06/2025"""
 
 l = [1,8,7,2,21,15]
